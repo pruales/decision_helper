b693665bc0e09b849af459e7e3f3eec21ef33d48
cruales <christie.ruales@gmail.com>
2017-11-11 22:28:50 -0500

    added commitment variable

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const NAME_ACTION = 'make_decision';
 // b. the parameters that are parsed from the make_name intent 
 const NAME_ARGUMENT = 'given-name';
 const TIME_PERIOD_ARGUMENT = 'time_period';
+const COMMITMENT_ARGUMENT = 'commitment_level';
 
 
 exports.decisionMaker = functions.https.onRequest((request, response) => {
@@ -21,7 +22,10 @@ exports.decisionMaker = functions.https.onRequest((request, response) => {
   function makeDecision (app) {
     let name = app.getArgument(NAME_ARGUMENT);
     let timePeriod = app.getArgument(TIME_PERIOD_ARGUMENT);
-    app.tell('Alright '+name+','+' you have thought about this for ' + timePeriod + ' days!');
+    let commitmentLevel = app.getArgument(COMMITMENT_ARGUMENT);
+    app.tell('Alright '+name+','+' you have thought about this for ' + timePeriod + ' days!' +
+        'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel);
+
   }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
