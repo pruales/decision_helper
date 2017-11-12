9c82f3d21b44059bb5746d0b855d70a56c401d7c
Paul Ruales <paul-ruales@uiowa.edu>
2017-11-12 04:56:45 -0500

    AI bois

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,11 @@
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Instantiates a client
+
 
 // a. the action name from the make_name Dialogflow intent
 const NAME_ACTION = 'make_decision';
@@ -17,16 +22,18 @@ const OPPOSITE_RISKS_ARGUMENT = 'opposite_risks';
 const TIME_EVALUATION_ARGUMENT = 'right_time';
 const PAST_EXPERIENCES_ARGUMENT = 'past_experiences';
 const FUN_ARGUMENT = 'determine_fun';
-
+const SENTIMENT_ARGUMENT = 'sentiment';
 
 exports.decisionMaker = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
+  const client = new language.LanguageServiceClient();
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
 
 
 // c. The function that evaluated decion state
   function makeDecision (app) {
+    let sentiment = app.getArgument(SENTIMENT_ARGUMENT);
     let name = app.getArgument(NAME_ARGUMENT);
     let timePeriod = app.getArgument(TIME_PERIOD_ARGUMENT);
     let commitmentLevel = app.getArgument(COMMITMENT_ARGUMENT);
@@ -38,184 +45,210 @@ exports.decisionMaker = functions.https.onRequest((request, response) => {
     let pastExperience = app.getArgument(PAST_EXPERIENCES_ARGUMENT);
     let determineFun = app.getArgument(FUN_ARGUMENT);
 
+    let text = sentiment;
 
-    if(correctTime=='Yes'){
-        var answerTime = 'a good';
-    }
-    else if(correctTime=='No'){
-        var answerTime = 'not a good';
-    }
-      //********************
-      var x =0;
-      var timeString = timePeriod.unit;
-      console.log(timeString);
+    response = '';
+
+    const document = {
+  	content: text,
+  	type: 'PLAIN_TEXT',
+    };
+
+      // Detects the sentiment of the text
+      //
+      client
+          .analyzeSentiment({document: document})
+          .then(results => {
+              const sentiment = results[0].documentSentiment;
+
+              console.log(`Text: ${text}`);
+              console.log(`Sentiment score: ${sentiment.score}`);
+              console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+
+              response+=`We sensed you sentiment to be ${sentiment.score}. `;
+
+              if(correctTime=='Yes'){
+                  var answerTime = 'a good';
+              }
+              else if(correctTime=='No'){
+                  var answerTime = 'not a good';
+              }
+              //********************
+              var x = 0;
+              var timeString = timePeriod.unit;
+              console.log(timeString);
+              console.log(timeString.indexOf("day") !== -1);
 // ----SECONDS MINUTES HOURS
-      if ((timeString.indexOf("s(s)") !== -1) || (timeString.indexOf("minutes") !== -1) ||
-          (timeString.indexOf("hours") !== -1)){
-          x = 0;
-          if (commitmentLevel >= 8){
-              x+=1;
-          }
-          if (physicalFeeling !== null){
-              x+=0;
-          }
-          if (goalAlignment !== null){
-              var goalString = goalAlignment;
-              if (goalString.indexOf("not") !== -1 || goalString.indexOf("unsure") !== -1 ||
-                  goalString.indexOf("don't")!==-1){
-                  x+=0;
-              }
-              else{
-                  x+=1;
-              }
-          }
-          if (riskLevel < reverseRisk){
-              x+=1;
-          }
-          if (correctTime !== null){
-              if (correctTime.indexOf("yes")!==-1){
-                  x+=1;
-              }
-          }
-          if (pastExperience.indexOf("positive")!==-1){
-              x+=1;
-          }
-          if (determineFun.indexOf("yes")!==-1){
-              x+=1;
-          }
+              if ((timeString == "s(s)") || (timeString == "min") ||
+                  (timeString.indexOf("hours") !== -1)) {
 
-      //********************
+                  if (commitmentLevel >= 8) {
+                      x += 1;
+                  }
+                  if (physicalFeeling !== null) {
+                      x += 0;
+                  }
+                  if (goalAlignment !== null) {
+                      var goalString = goalAlignment;
+                      if (goalString.indexOf("not") !== -1 || goalString.indexOf("unsure") !== -1 ||
+                          goalString.indexOf("don't") !== -1) {
+                          x += 0;
+                      }
+                      else {
+                          x += 1;
+                      }
+                  }
+                  if (riskLevel < reverseRisk) {
+                      x += 1;
+                  }
+                  if (correctTime !== null) {
+                      if (correctTime.indexOf("yes") !== -1) {
+                          x += 1;
+                      }
+                  }
+                  if (pastExperience.indexOf("positive") !== -1) {
+                      x += 1;
+                  }
+                  if (determineFun.indexOf("yes") !== -1) {
+                      x += 1;
+                  }
+              }
 
+                  //********************
 
 // ----DAYS WEEKS
-      else if ((timeString.indexOf("days") !== -1) || (timeString.indexOf("weeks") !== -1)){
-          x = 0;
-          if (commitmentLevel >= 7){
-              x+=1;
-          }
-          if (physicalFeeling !== null){
-              x+=0;
-          }
-          if (goalAlignment !== null){
-              if (goalAlignment.indexOf("not") !== -1 || goalAlignment.indexOf("unsure") !== -1 ||
-                  goalAlignment.indexOf("don't")!==-1){
-                  x+=0;
-              }
-              else{
-                  x+=1;
-              }
-          }
-          if (riskLevel < reverseRisk){
-              x+=1;
-          }
-          if (correctTime !== null){
-              if (correctTime.indexOf("yes")!==-1){
-                  x+=1;
-              }
-          }
-          if (pastExperience.indexOf("positive")!==-1){
-              x+=1;
-          }
-          if (determineFun.indexOf("yes")!==-1){
-              x+=1;
-          }
+              else if ((timeString.indexOf("day") !== -1) || (timeString.indexOf("wk") !== -1)){
+                  console.log('day');
+                  console.log(`comitmentlevel: ${commitmentLevel}`)
+                  if (commitmentLevel >= 7){
+                      x+=1;
+                      console.log('commitment greater than 7');
+                  }
+                  console.log(`phys feeling: ${physicalFeeling}`)
+                  if (physicalFeeling !== null){
+                      x+=0;
+                  }
+                  console.log(`goalAlign: ${goalAlignment}`)
+                  if (goalAlignment !== null){
+                      if (goalAlignment.indexOf("not") !== -1 || goalAlignment.indexOf("unsure") !== -1 ||
+                          goalAlignment.indexOf("don't")!==-1){
+                          x+=0;
+                      }
+                      else{
+                          x+=1;
+                      }
+                  }
+                  console.log(`risk level: ${riskLevel}`)
+                  console.log(`reverse risk: ${reverseRisk}`)
+                  if (riskLevel < reverseRisk){
+                      x+=1;
+                  }
+                  console.log(`correct Time: ${correctTime}`)
+                  if (correctTime !== null){
+                      if (correctTime.indexOf("yes")!==-1){
+                          x+=1;
+                      }
+                  }
+                  console.log(`pastExp: ${pastExperience}`)
 
-      }
+                  if (pastExperience.indexOf("positive")!==-1){
+                      x+=1;
+                  }
+                  console.log(`determineFun: ${determineFun}`)
+                  if (determineFun.indexOf("yes")!==-1){
+                      x+=1;
+                  }
+
+                  console.log(`x: ${x}`)
+              }
 
 // ----MONTHS YEARS
-      else if ((timeString.indexOf("months") !== -1) || (timeString.indexOf("yr") !== -1)){
-          x = 1;
-          if (commitmentLevel >= 6){
-              x+=1;
-          }
-          if (physicalFeeling !== null){
-              x+=0;
-          }
-          if (goalAlignment !== null){
-              if (goalAlignment.indexOf("not") !== -1 || goalAlignment.indexOf("unsure") !== -1 ||
-                  goalAlignment.indexOf("don't")!==-1){
-                  x+=0;
+              else if ((timeString.indexOf("mo") !== -1) || (timeString.indexOf("yr") !== -1)) {
+                  x += 1;
+                  if (commitmentLevel >= 6) {
+                      x += 1;
+                  }
+                  if (physicalFeeling !== null) {
+                      x += 0;
+                  }
+                  if (goalAlignment !== null) {
+                      if (goalAlignment.indexOf("not") !== -1 || goalAlignment.indexOf("unsure") !== -1 ||
+                          goalAlignment.indexOf("don't") !== -1) {
+                          x += 0;
+                      }
+                      else {
+                          x += 1;
+                      }
+                  }
+                  if (riskLevel < reverseRisk) {
+                      x += 1;
+                  }
+                  if (correctTime.indexOf("yes") !== -1) {
+                      x += 1;
+                  }
+
+                  if (pastExperience.indexOf("positive") !== -1) {
+                      x += 1;
+                  }
+                  if (determineFun.indexOf("yes") !== -1) {
+                      x += 1;
+                  }
               }
-              else{
-                  x+=1;
+
+
+              else {
+                  response += 'You should think about it more. ' + x + ' variable. '
+                      + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
+                      'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
+                      'You feel ' + physicalFeeling + ' about this. ' +
+                      'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
+                      'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
+                      'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
+                      'You said now is ' + correctTime + ' time for this. ' +
+                      'You said your past experience with this is ' + pastExperience + '. ' +
+                      'When asked if this decision could be fun you said '+ determineFun + '!';
+
               }
-          }
-          if (riskLevel < reverseRisk){
-              x+=1;
-          }
-          if (correctTime.indexOf("yes")!==-1){
-                  x+=1;
+
+              if (x > 4){
+                  response+='Sounds like a good idea to me! ' + x + ' variable. '
+                      + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
+                      'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
+                      'You feel ' + physicalFeeling + ' about this. ' +
+                      'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
+                      'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
+                      'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
+                      'You said now is ' + correctTime + ' time for this. ' +
+                      'You said your past experience with this is ' + pastExperience + '. ' +
+                      'When asked if this decision could be fun you said '+ determineFun + '!';
+
               }
-          }
-          if (pastExperience.indexOf("positive")!==-1){
-              x+=1;
-          }
-          if (determineFun.indexOf("yes")!==-1){
-              x+=1;
-          }
-      }
-
-
-      else {
-          app.tell('You should think about it more. ' + x + ' variable. '
-          + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
-              'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
-              'You feel ' + physicalFeeling + ' about this. ' +
-              'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
-              'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
-              'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
-              'You said now is ' + correctTime + ' time for this. ' +
-              'You said your past experience with this is ' + pastExperience + '. ' +
-              'When asked if this decision could be fun you said '+ determineFun + '!');
-      }
-
-      if (x > 4){
-          app.tell('Sounds like a good idea to me! ' + x + ' variable. '
-              + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
-              'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
-              'You feel ' + physicalFeeling + ' about this. ' +
-              'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
-              'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
-              'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
-              'You said now is ' + correctTime + ' time for this. ' +
-              'You said your past experience with this is ' + pastExperience + '. ' +
-              'When asked if this decision could be fun you said '+ determineFun + '!');
-      }
-      else {
-          app.tell('This might not be the best decision. Maybe try thinking about it more! ' + x + ' variable. '
-              + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
-              'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
-              'You feel ' + physicalFeeling + ' about this. ' +
-              'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
-              'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
-              'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
-              'You said now is ' + correctTime + ' time for this. ' +
-              'You said your past experience with this is ' + pastExperience + '. ' +
-              'When asked if this decision could be fun you said '+ determineFun + '!');
-      }
-
-      if (x > 4){
-          app.tell('Sounds like a good idea to me! ' + x + ' variable');
-      }
-      else {
-          app.tell('This might not be the best decision. Maybe try thinking about it more! ' + x + ' variable');
-
-      }
+              else {
+                  response+='This might not be the best decision. Maybe try thinking about it more! ' + x + ' variable. '
+                      + 'Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
+                      'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
+                      'You feel ' + physicalFeeling + ' about this. ' +
+                      'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
+                      'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
+                      'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
+                      'You said now is ' + correctTime + ' time for this. ' +
+                      'You said your past experience with this is ' + pastExperience + '. ' +
+                      'When asked if this decision could be fun you said '+ determineFun + '!';
+              }
 
 
 
 //*********************
 
 
-      app.tell('Alright '+name+','+' you have thought about this for ' + timePeriod.amount + ' ' + timePeriod.unit + '(s). ' +
-        'On a scale of 1-10 you have rated your commitment as a ' +  commitmentLevel + '. ' +
-        'You feel ' + physicalFeeling + ' about this. ' +
-        'This decision is ' + goalAlignment + ' aligned with your long-term goals. ' +
-        'On a scale of 1-10 you rated the risks of this decision at a ' + riskLevel + '. ' +
-        'You rated the risks of not doing it a ' + reverseRisk + ' on a scale of 1-10. ' +
-        'You said now is ' + correctTime + ' time for this. ' +
-        'You said your past experience with this is ' + pastExperience + '. ' +
-        'When asked if this decision could be fun you said '+ determineFun + '!');
+              app.tell(response);
+          })
+          .catch(err => {
+              app.tell('your trash!');
+              console.error('ERROR:', err);
+          });
+
+
 
   }
   // d. build an action map, which maps intent names to functions
